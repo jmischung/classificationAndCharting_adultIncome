c4c556578382ad6bba330f208a038278154eee5c
jmischung <josh@knoasis.io>
2020-06-30 21:41:18 -0700

    Change inputs to match selected model features, style elements, add all functionality upto hooking up the model

--- adult_app.py
+++ adult_app.py
@@ -8,7 +8,7 @@ import dash_html_components as html
 import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output, State
 
-# App Layout
+# Components
 # ----------
 
 # Set stylesheetgs
@@ -17,110 +17,168 @@ yeti = dbc.themes.YETI
 # Initialize app
 app = dash.Dash(__name__, external_stylesheets=[yeti])
 
-# Input components
-dropdown_sex = dbc.FormGroup([
-	dbc.Label("Sex", html_for="dropdown_sex", style={"fontWeight": "bold"}),
-	dbc.Col(
-		dbc.Select(
-		id="dropdown_sex",
-		options=[
-			{"label": "Male", "value": "Male"},
-			{"label": "Female", "value": "Female"}
-		]
-	),
-	width=6
-	)
-])
+# Inputs
 
 slider_age = dbc.FormGroup([
-	dbc.Label("Age", html_for="slider_age", style={"fontWeight": "bold"}),
 	dbc.Col(
-		dcc.Slider(
-			id="slider_age",
-			min=17, max=90, step=1, value=53,
-			marks={
-				17: {"label": "17"},
-				90: {"label": "90"}
-			},
-			tooltip={"always_visible": False, "placement": "bottom"}
-		),
+		[
+			dbc.Label("Age", html_for="slider_age", style={"fontWeight": "bold"}),
+			dcc.Slider(
+				id="slider_age",
+				min=17, max=90, step=1, value=53,
+				marks={
+					17: {"label": "17"},
+					90: {"label": "90"}
+				},
+				tooltip={"always_visible": False, "placement": "bottom"}
+			)
+		],
 		width=12
 	)
 ])
 
-dropdown_workclass = dbc.FormGroup([
-	dbc.Label("Workclass", html_for="dropdown_workclass", style={"fontWeight": "bold"}),
+dropdown_education = dbc.FormGroup([
 	dbc.Col(
-		dbc.Select(
-		id="dropdown_workclass",
-		options=[
-			{"label": "State-gov", "value": "State-gov"},
-			{"label": "Self-emp-not-inc", "value": "Self-emp-not-inc"},
-			{"label": "Private", "value": "Private"},
-			{"label": "Federal-gov", "value": "Federal-gov"},
-			{"label": "Local-gov", "value": "Local-gov"},
-			{"label": "Self-emp-inc", "value": "Self-emp-inc"},
-			{"label": "Without-pay", "value": "Without-pay"},
-			{"label": "Never-worked", "value": "Never-worked"},
-		]
-	),
-	width=6
+		[
+			dbc.Label("Education", html_for="dropdown_education", style={"fontWeight": "bold"}),
+			dbc.Select(
+			id="dropdown_education",
+			options=[
+				{"label": "Preschool", "value": " Preschool"},
+				{"label": "1st-4th", "value": " 1st-4th"},
+				{"label": "5th-6th", "value": " 5th-6th"},
+				{"label": "7th-8th", "value": " 7th-8th"},
+				{"label": "9th", "value": " 9th"},
+				{"label": "10th", "value": " 10th"},
+				{"label": "11th", "value": " 11th"},
+				{"label": "12th", "value": " 12th"},
+				{"label": "High School Graduate", "value": " HS-grad"},
+				{"label": "Some College", "value": " Some-college"},
+				{"label": "Professional School", "value": " Prof-school"},
+				{"label": "Associate - Vocational", "value": " Assoc-voc"},
+				{"label": "Associate - Academic", "value": " Assoc-acdm"},
+				{"label": "Bachelors", "value": " Bachelors"},
+				{"label": "Masters", "value": " Masters"},
+				{"label": "Doctorate", "value": " Doctorate"},
+			]
+		)
+	],
+	width=8
 	)
 ])
 
-dropdown_education = 1
-
 radio_marital_status = dbc.FormGroup([
-	dbc.Label("Marital Status", html_for="radio_marital_status", style={"fontWeight": "bold"}),
 	dbc.Col(
-		dbc.RadioItems(
-			id="radioitems-input",
-			className="form-check",
-			labelClassName="form-check-label",
-			inputClassName="form-check-input",
-			options=[
-				{"label": "Never-married", "value": "Never-married"},
-				{"label": "Married-civ-spouse", "value": "Married-civ-spouse"},
-				{"label": "Divorced", "value": "Divorced"},
-				{"label": "Married-spouse-absent", "value": "Married-spouse-absent"},
-				{"label": "Separated", "value": "Separated"},
-				{"label": "Married-AF-spouse", "value": "Married-AF-spouse"},
-				{"label": "Widowed", "value": "Widowed"},
-			]
-	),
+		[
+			dbc.Label("Marital Status", html_for="radio_marital_status", style={"fontWeight": "bold"}),
+			dbc.RadioItems(
+				id="radioitems-input",
+				className="form-check",
+				labelClassName="form-check-label",
+				inputClassName="form-check-input",
+				options=[
+					{"label": "Never married", "value": "Never-married"},
+					{"label": "Married, civil-spouse", "value": "Married-civ-spouse"},
+					{"label": "Married, spouse absent", "value": "Married-spouse-absent"},
+					{"label": "Married, armed forces spouse", "value": "Married-AF-spouse"},
+					{"label": "Separated", "value": "Separated"},
+					{"label": "Divorced", "value": "Divorced"},
+					{"label": "Widowed", "value": "Widowed"},
+				]
+			)
+		]
 	)],
 	className="form-group"
 )
 
-button_run = dbc.Button("Run", color="primary", style={"margin-bottom": "10px"})
-
-display_probability = html.Div(
-	[
-		html.H3(
-			"Probability",
-			style={
-				"fontWeight": "bold",
-				"textDecoration": "underline"
-			}
-		),
-		html.H1("87.38%")
-	],
-	style={"marginLeft": "10px"}
-)
-
-card_content = [
-	dbc.CardHeader("Probability", style={"fontWeight": "bold"}),
-	dbc.CardBody([
-		html.H2("87.38%", className="card-title"),
-		html.P(
-			"Do I want to write a few things here...",
-			className="card-text"
-		)
-	])
+dropdown_relationship_items = [
+	dbc.DropdownMenuItem("Not-in-family", id=" Not-in-family"),
+	dbc.DropdownMenuItem("Husband", id=" Husban"),
+	dbc.DropdownMenuItem("Wife", id=" Wife"),
+	dbc.DropdownMenuItem("Own-child", id=" Own-child"),
+	dbc.DropdownMenuItem("Unmarried", id=" Unmarried"),
+	dbc.DropdownMenuItem("Other-relative", id=" Other-relative")
 ]
 
-# App layout
-form = dbc.Form([dropdown_sex, slider_age, dropdown_workclass, radio_marital_status, button_run])
+dropdown_relationship = dbc.FormGroup(
+	[
+		dbc.Col(
+			[
+				dbc.InputGroup([
+					dbc.DropdownMenu(
+						dropdown_relationship_items,
+						className="input-group-prepend",
+						label="Relationship",
+						addon_type="prepend"
+					),
+					dbc.Input(id="dropdown_relationship_input", className="form-control")
+				]),
+			],
+			width=10
+		),
+	],
+	className="form-group"
+)
+
+dropdown_occupation = dbc.FormGroup([
+	dbc.Col(
+		[
+			dbc.Label("Occupation", html_for="dropdown_occupation", style={"fontWeight": "bold"}),
+			dbc.Select(
+				id="dropdown_occupation",
+				options=[
+					{"label": "State-gov", "value": "State-gov"},
+					{"label": "Self-emp-not-inc", "value": "Self-emp-not-inc"},
+					{"label": "Private", "value": "Private"},
+					{"label": "Federal-gov", "value": "Federal-gov"},
+					{"label": "Local-gov", "value": "Local-gov"},
+					{"label": "Self-emp-inc", "value": "Self-emp-inc"},
+					{"label": "Without-pay", "value": "Without-pay"},
+					{"label": "Never-worked", "value": "Never-worked"},
+				]
+			)
+		],
+		width=8
+	)
+])
+
+input_hours = dbc.FormGroup([
+	dbc.Col(
+		[
+			dbc.Label("Hours Per Week", className="control-label", html_for="input_hours",
+					  style={"fontWeight": "bold"}),
+			dbc.InputGroup([
+				dbc.Input(id="input_hours", className="form-control", type="number", min=1, max=99, step=1),
+				dbc.InputGroupAddon("avg. hrs/wk", className="input-group-append", addon_type="append")
+			]),
+			dbc.FormText("Must be a whole number from 1-99")
+		],
+		width=8
+	)
+])
+
+button_run = dbc.Col(
+	dbc.Button("Run", id="button_run", color="primary", style={"margin-bottom": "10px"})
+)
+
+output_card = dbc.Card(
+	[
+		dbc.CardHeader("Probability", style={"fontWeight": "bold"}),
+		dbc.CardBody([
+			html.H2("0", id="output_probability", className="card-title"),
+			html.P(
+				"Do I want to write a few things here...",
+				className="card-text"
+			)
+		])
+	],
+	color="light",
+	outline=True
+)
+
+# Layout
+form = dbc.Form([slider_age, dropdown_education, radio_marital_status, dropdown_relationship,
+				 dropdown_occupation, input_hours, button_run])
 app.layout = dbc.Container(
 	[
 		html.H1("Adult Income | Probability of Earning More Than $50K Per Year"),
@@ -128,12 +186,67 @@ app.layout = dbc.Container(
 		dbc.Row(
 			[
 				dbc.Col(form, md=4),
-				dbc.Col(dbc.Card(card_content, color="light", outline=True), md=4)
+				dbc.Col(output_card, id="output_card", md=4)
 			],
 		)
 	],
 	fluid=True
 )
+
+# Interactivity
+@app.callback(
+	Output("dropdown_relationship_input", "value"),
+	[
+		Input(" Not-in-family", "n_clicks"),
+		Input(" Husban", "n_clicks"),
+		Input(" Wife", "n_clicks"),
+		Input(" Own-child", "n_clicks"),
+		Input(" Unmarried", "n_clicks"),
+		Input(" Other-relative", "n_clicks"),
+	]
+)
+def dropdown_menu(r1, r2, r3, r4, r5, r6):
+	context = dash.callback_context
+
+	if not context.triggered:
+		return ""
+	else:
+		menu_item = context.triggered[0]["prop_id"].split(".")[0]
+		return menu_item
+
+
+@app.callback(
+	Output("output_probability", "children"),
+	[Input("button_run", "n_clicks")],
+	state=[
+		State("slider_age", "value"),
+		State("dropdown_education", "value"),
+		State("radioitems-input", "value"),
+		State("dropdown_relationship_input", "value"),
+		State("dropdown_occupation", "value"),
+		State("input_hours", "value")
+	]
+)
+def run_model(n_clicks, age, education, marital, relationship, occupation, hours):
+	inputs = [age, education, marital, relationship, occupation, hours]
+	context = dash.callback_context
+
+	if not context.triggered:
+		return "0"
+	elif None in inputs:
+		return dbc.Alert(
+			[
+				html.H5("Oops!", className="alert-heading"),
+				html.P("One or more of the inputs hasn't been completed...", className="mb-0")
+			],
+			className="alert alert-dismissable alert-danger",
+			color="danger",
+			dismissable=True,
+			is_open=True,
+			duration=6000)
+	else:
+		return age
+
 
 # Main block
 if __name__ == '__main__':
